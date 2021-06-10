d55426a87ebe305fdd4e881640ca188e42eff0ad
robaElshazly <roba1818@gmail.com>
2021-06-10 18:41:10 +1000

    update document format

--- src/Components/Finish.js
+++ src/Components/Finish.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
-export default function Finish(props){
-    console.log("from finish component")
-return(
+export default function Finish(props) {
+  console.log("from finish component");
+  return (
     <>
-    <p>Finish</p>
-    <p>Score: {props.score}</p>
-    <p>Accuracy: {props.accuracy}%</p>
-    <p>WMP: {props.wordsPerMin}</p>
-    <button onClick={()=>{props.setGameStart("start")}}>Replay</button>
+      <p>Finish</p>
+      <p>Score: {props.score}</p>
+      <p>Accuracy: {props.accuracy}%</p>
+      <p>WMP: {props.wordsPerMin}</p>
+      <button
+        onClick={() => {
+          props.setGameStart("start");
+        }}
+      >
+        Replay
+      </button>
     </>
-)
+  );
 }
