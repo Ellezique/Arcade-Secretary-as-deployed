1adc75815f115db4bee0647e2b041b09092e5519
Ellezique <gizellevanzyl@gmail.com>
2021-06-10 11:39:20 +1000

    Complete NavBar.js (no styling yet) to get routing to work, add burger and drink icons

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,28 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
 
 function NavBar() {
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+    const closeBurgerMenu = () => setClick(false)
+
     return (
-        <div>
-            <h1>NAVBAR</h1>
-            <ul>
-                <li>
-                    <Link>
-                        HOME
-                    </Link>
-                </li>
-                <li>
-                    <Link>
-                        RULES
-                    </Link>
-                </li>
-                <li>
-                    <Link>
-                        CONTACT
-                    </Link>
-                </li>
-            </ul>
-        </div>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to='/' className="logo-section">
+                 <i className="typewriter" src="./public/typewriterlogo.png"/>
+                  ARCADE 
+                 <i class="far fa-keyboard"/> 
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fab fa-gulp' : 'fas fa-hamburger'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeBurgerMenu}>
+                            HOME
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/Rules' className='nav-links' onClick={closeBurgerMenu}>
+                            RULES
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/Contact' className='nav-links' onClick={closeBurgerMenu}>
+                            CONTACT
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
